6dee9498b1e644b091900f67b782be300630798e
Benjamin Cohn <cohnbj13@gmail.com>
2019-05-30 00:03:54 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,8 @@
-# Define display_board that accepts a board and prints
+
+def display_board(board)
+  puts" #{board[0]}  | #{board[1]}  | #{board[2]}  "
+  puts"-----------"
+  puts"   |   |   "
+  puts"-----------"
+  puts"   |   |   "# Define display_board that accepts a board and prints
 # out the current state.
